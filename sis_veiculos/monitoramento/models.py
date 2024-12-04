2a2dafee36b4bfa748ca452cc38e2a1cb6a80c7a
Valdeir027 <saparavaldeir@gmail.com>
2024-12-03 21:05:20 -0400

    Funcionalidade de vagas aplicada

--- sis_veiculos/monitoramento/models.py
+++ sis_veiculos/monitoramento/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch.dispatcher import receiver
+from datetime import datetime
 
 class Veiculo(models.Model):
     placa = models.CharField(max_length=7, unique=True)
@@ -9,6 +12,7 @@ class Veiculo(models.Model):
     def __str__(self):
         return f"{self.marca} {self.modelo} -  {self.placa}"
 
+
 class Motorista(models.Model):
     nome = models.CharField(max_length=100)
     cpf = models.CharField(max_length=11, unique=True)
@@ -17,19 +21,49 @@ class Motorista(models.Model):
     def __str__(self):
         return self.nome
 
+
 class Vaga(models.Model):
-    numero = models.PositiveIntegerField(unique=True)
-    status = models.BooleanField(default=True)  # True = Disponível, False = Ocupada
+    numero = models.PositiveIntegerField(unique=True, blank=True, null=True)
+    veiculo = models.OneToOneField(Veiculo, blank=True, null=True, on_delete=models.DO_NOTHING, related_name='vaga')
+    motorista =  models.OneToOneField(Motorista, blank=True, null=True, on_delete=models.DO_NOTHING, related_name='vaga')
+
+    def ocupar(self, veiculo: Veiculo, motorista: Motorista):
+        self.veiculo = veiculo
+        self.motorista = motorista
+        self.save()
+        return self
+    
+    def desocupar(self):
+        if self.veiculo and self.motorista:
+            self.veiculo = None
+            self.motorista = None
+            self.save()
+            return self
+        return "Vaga não está ocupada"
 
     def __str__(self):
-        return f"Vaga {self.numero} - {'Disponível' if self.status else 'Ocupada'}"
+        return f"Vaga {self.numero} - {'Ocupada' if self.veiculo else 'Disponivel'}"
+
 
 class EntradaSaida(models.Model):
     veiculo = models.ForeignKey(Veiculo, on_delete=models.CASCADE)
-    motorista = models.ForeignKey(Motorista, on_delete=models.CASCADE)
+    motorista = models.ForeignKey(Motorista, on_delete=models.CASCADE,)
     vaga = models.ForeignKey(Vaga, on_delete=models.CASCADE)
     entrada = models.DateTimeField(auto_now_add=True)
     saida = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.veiculo} - Entrada: {self.entrada} - Saída: {self.saida or 'Em andamento'}"
+
+
+
+
+@receiver(post_save, sender=Vaga)
+def gerar_numero_vaga(sender, instance, created, **kwargs):
+    if created:
+        data = datetime.now()
+        numero = f"{data.year}{data.month}{data.day}0{data.second}"
+        instance.numero = int(numero)
+        instance.save()
+
+post_save.connect(gerar_numero_vaga, sender=Vaga)
--- sis_veiculos/monitoramento/views.py
+++ sis_veiculos/monitoramento/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import MotoristaForm
-from .models import Veiculo, Motorista
+from .models import Veiculo, Motorista, Vaga
+
 
 def index(request):
     return render(request, 'monitoramento/index.html')
+
 
 # Página principal do CRUD
 def crud_veiculos(request):
     veiculos = Veiculo.objects.all()
     return render(request, 'monitoramento/veiculos.html', {'veiculos': veiculos})
+
 
 # Adicionar um novo veículo
 def adicionar_veiculo(request):
@@ -20,6 +23,7 @@ def adicionar_veiculo(request):
         Veiculo.objects.create(placa=placa, marca=marca, modelo=modelo, cor=cor)
         return redirect('crud_veiculos')
     return render(request, 'monitoramento/veiculos_add.html')
+
 
 # Atualizar veículo existente
 def atualizar_veiculo(request, veiculo_id):
@@ -33,16 +37,19 @@ def atualizar_veiculo(request, veiculo_id):
         return redirect('crud_veiculos')
     return render(request, 'monitoramento/veiculos_edit.html', {'veiculo': veiculo})
 
+
 # Excluir um veículo
 def excluir_veiculo(request, veiculo_id):
     veiculo = get_object_or_404(Veiculo, id=veiculo_id)
     veiculo.delete()
     return redirect('crud_veiculos')
 
+
 # Listar motoristas
 def crud_motoristas(request):
     motoristas = Motorista.objects.all()
     return render(request, 'monitoramento/motoristas.html', {'motoristas': motoristas})
+
 
 # Adicionar motorista
 def motoristas_add(request):
@@ -54,6 +61,66 @@ def motoristas_add(request):
     else:
         form = MotoristaForm()
     return render(request, 'monitoramento/motoristas_add.html', {'form': form})
+
+
+def crud_vagas(request):
+    vagas = Vaga.objects.all()
+    return render(request, 'monitoramento/vagas.html', context={
+        'vagas': vagas,
+    })
+
+
+def vagas_ocopar(request, numero):
+    vaga = get_object_or_404(Vaga, numero=numero)
+    error = None
+
+    if request.method == 'POST':
+        data = request.POST
+
+        try:
+            veiculo = get_object_or_404(Veiculo, id=data.get('veiculo'))
+
+            motorista = get_object_or_404(Motorista, cpf=data.get('cpf'))
+            print(motorista)
+            vaga.ocupar(veiculo=veiculo, motorista=motorista)
+
+            return redirect(crud_vagas)
+        except:
+            error = F"Erro ao salvar"
+    veiculos = Veiculo.objects.filter(vaga=None)
+    return render(request, 'monitoramento/vagas_ocupar.html', {
+        'vaga': vaga,
+       'veiculos':veiculos,
+       'error':error
+    })
+
+def vagas_desocupar(request, numero):
+    vaga = get_object_or_404(Vaga, numero=numero)
+    if request.method =="POST":
+        vaga.desocupar()
+        return redirect(crud_vagas)
+
+    return render(request, 'monitoramento/vagas_desocupar.html', {
+        'vaga': vaga,
+    })
+    
+
+def vagas_editar(request, numero):
+    vaga = get_object_or_404(Vaga, numero=numero)
+    error = None
+
+    if request.method == 'POST':
+        data = request.POST
+
+        
+        vaga.veiculo = veiculo
+        vaga.motorista = motorista
+        vaga.save()
+        return redirect(crud_vagas)
+    return render(request, 'monitoramento/vagas_ocupar.html', {
+        'vaga': vaga,
+       'error':error
+    })
 
 # Editar motorista
 def motoristas_edit(request, id):
